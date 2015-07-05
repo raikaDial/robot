14fef4234fef73c3c023a934fa4baabce0d4da22
Ryker Dial <rldial@alaska.edu>
2015-07-04 20:50:46 -0800

    Updated robot firmware to use a servo for steering.

--- robotweb/firmware/firmware.ino
+++ robotweb/firmware/firmware.ino
@@ -1,19 +1,28 @@
-// Sabertooth 2x5 controls the two motors with one 8 byte character.
-// Sending a character between 1 and 127 controls motor 1.
-// Sending a character between 128 and 255 controls 
-#define RIGHT 1
-#define CENTER 64
-#define LEFT 127
-#define BACKWARD 128
-#define STOP 192
-#define FORWARD 255
-
+// Sabertooth 2x5 controls the drive motor with one 8 byte character.
+// Sending a character between 1 and 127 controls the speed and direction of the motor.
+// Backward is 0, Forward is 127, and 64 is Stop
+#define BACKWARD 0
+#define STOP 64
+#define FORWARD 127
 #define RANGE 63
+
+// Servo used to steer the vehicle. Center is 90 degrees, full left is 0 degrees,
+// full right is 180 degrees
+#define LEFT 0
+#define CENTER 90
+#define RIGHT 180
 
 // LED Pins
 #define RED_LED_PIN 8
 #define GREEN_LED_PIN 9
 #define BLUE_LED_PIN 10
+
+// Steering Servo Pin
+#define STEERING_PIN 13
+
+#include <Servo.h>
+
+Servo steering;
 
 // Drive power for robot. Low by default.
 int drivePower = 14;
@@ -24,7 +33,9 @@ void setup()
     // Serial1 is used for communication between arduino and sabertooth
     Serial.begin(9600);
     Serial1.begin(9600);
-
+    
+    steering.attach( STEERING_PIN );
+    steering.write( CENTER );
     pinMode(RED_LED_PIN, OUTPUT);
     pinMode(BLUE_LED_PIN, OUTPUT);
     pinMode(GREEN_LED_PIN, OUTPUT);
@@ -48,13 +59,13 @@ void loop()
             Serial1.write( FORWARD - RANGE + drivePower );
             break;
           case 'l':
-            Serial1.write( LEFT );
+            steering.write( LEFT );
             break;
           case 'c':
-            Serial1.write( CENTER );
+            steering.write( CENTER );
             break;
           case 'r':
-            Serial1.write( RIGHT );
+            steering.write( RIGHT );
             break;
           case 'b':
             Serial1.write( BACKWARD + (RANGE + 1) - drivePower );
